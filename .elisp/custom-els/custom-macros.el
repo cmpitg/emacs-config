4a793f5c311ac3083237c73606baa2282e0c49a2
Duong H. Nguyen <cmpitg@gmail.com>
2013-02-24 22:09:09 +0700

    Remove old config

--- .elisp/custom-els/custom-macros.el
+++
@@ -1,5 +0,0 @@
-
-
-(fset 'c-make-prototype
-   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([1 67108896 5 134217847 16 16 14 14 18 105 110 116 32 109 97 105 110 13 16 return 25 return 16 134217820] 0 "%d")) arg)))
-

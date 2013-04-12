906d87682bed5f83ba04d28426698766c5a8e920
Duong H. Nguyen <cmpitg@gmail.com>
2013-04-12 16:31:01 +0700

    Add filter to file browser

--- bin/filebrowser-emacs.py
+++ bin/filebrowser-emacs.py
@@ -4,16 +4,25 @@
 # TODO:
 #
 # * Passing directory path
-# * File filter
+#
+# * Shortcut:
+#   - <Ctrl L> to jump to path entry
+#   - <Ctrl F> to jump to filter entry
+#
+# * File filter (Done)
+#
+# * Expand path (Done)
 #
 
 
 import sys
 import os
+import re
+
 from PySide import QtCore, QtGui
 
 
-ROOT_PATH = "/home/cmpitg/Desktop/"
+ROOT_PATH = "~/Desktop/"
 
 
 class MyTreeView(QtGui.QTreeView):
@@ -26,8 +35,6 @@ class MyTreeView(QtGui.QTreeView):
         for idx in range(1, 4):
             self.hideColumn(idx)
         self.setAnimated(True)
-        self.setWindowTitle("Directory Tree View")
-        # self.setSelectionMode(QtGui.QAbstractItemView.ExtendedSelection)
 
     def createModel(self):
         model = QtGui.QFileSystemModel()
@@ -36,13 +43,41 @@ class MyTreeView(QtGui.QTreeView):
         self.setPath(ROOT_PATH)
 
     def setPath(self, path):
-        path = os.path.abspath(path)
+        path = expandPath(path)
 
         if not os.path.isdir(path):
             return
 
+        self.current_path = path
         self.model.setRootPath(path)
         self.setRootIndex(self.model.index(path))
+
+    def filterFiles(self, regex):
+        if regex == "" or regex == "*":
+            regex = ".*"
+
+        for idx in range(self.countItems()):
+            if not re.search(regex,
+                             self.getFileName(idx),
+                             flags=re.IGNORECASE):
+                self.setRowHidden(idx, self.getCurrentModelIndex(), True)
+            else:
+                self.setRowHidden(idx, self.getCurrentModelIndex(), False)
+
+    def getCurrentModelIndex(self):
+        return self.model.index(self.current_path)
+
+    def getFileName(self, number):
+        return self.model.fileName(self.getModelIndexOfItem(number))
+
+    def getModelIndexOfItem(self, number):
+        parent = self.getCurrentModelIndex()
+        index = self.model.index(number, 0, parent)
+        return index
+
+    def countItems(self):
+        index = self.getCurrentModelIndex()
+        return self.model.rowCount(index)
 
     def selectedItems(self):
         list = self.selectedIndexes()
@@ -70,23 +105,36 @@ class MainWindow(QtGui.QWidget):
     def __init__(self):
         super(MainWindow, self).__init__()
 
-        self.resize(800, 600)
+        self.resize(350, 650)
+        self.move(0, 0)
+        self.setWindowTitle("File Browser for Emacs Server")
+
         self.createChildren()
         self.show()
 
     def createChildren(self):
         box = QtGui.QVBoxLayout()
         self.createPathEntry()
+        self.createFilterEntry()
         self.createTreeView()
 
         self.pathEntry.textChanged.connect(self.tree.setPath)
+        self.pathEntry.textEdited.connect(self.clearFilterEntry)
+        self.filterEntry.textEdited.connect(self.tree.filterFiles)
 
         box.addWidget(self.pathEntry)
+        box.addWidget(self.filterEntry)
         box.addWidget(self.tree)
         box.setContentsMargins(0, 0, 0, 0)
 
         self.setLayout(box)
         self.box = box
+
+    def createFilterEntry(self):
+        fil = QtGui.QLineEdit()
+        fil.setText("*")
+
+        self.filterEntry = fil
 
     def createPathEntry(self):
         pathEntry = QtGui.QLineEdit()
@@ -98,6 +146,13 @@ class MainWindow(QtGui.QWidget):
         tree = MyTreeView()
 
         self.tree = tree
+
+    def clearFilterEntry(self, path):
+        self.filterEntry.setText("*")
+
+
+def expandPath(path):
+    return os.path.abspath(os.path.expanduser(path))
         
 
 app = QtGui.QApplication(sys.argv)

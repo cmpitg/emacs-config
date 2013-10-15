54f6796b661eeb393d4b0df5b34dfe71fcbae971
Duong H. Nguyen <cmpitg@gmail.com>
2013-10-15 16:05:47 +0700

    js3-mode sucks, back to js2-mode

--- emacs-prexiki.el
+++ emacs-prexiki.el
@@ -149,10 +149,15 @@
 ;; JavaScript
 ;;
 
-(require 'js3-mode)
-(setq js3-auto-indent-p t
-      js3-enter-indents-newline t
-      js3-indent-on-enter-key t)
+;; (require 'js3-mode)
+;; (setq js3-auto-indent-p t
+;;       js3-enter-indents-newline t
+;;       js3-indent-on-enter-key t)
+
+(require 'js2-mode)
+(add-hook 'js-mode-hook 'js2-minor-mode)
+($auto-load-mode '("\\.js\\'") 'js2-mode)
+(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
 
 (add-hook 'html-mode-hook '$auto-reload-firefox-after-save-hook)
 (add-hook 'css-mode-hook '$auto-reload-firefox-after-save-hook)

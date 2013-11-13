632b9df5c622d9b3b2d44757acc2d25558aaf999
Duong H. Nguyen <cmpitg@gmail.com>
2013-11-13 18:13:58 +0700

    Fix $move-to-beginning-of-line

--- emacs-cmpitg-config/custom-functions.el
+++ emacs-cmpitg-config/custom-functions.el
@@ -45,24 +45,13 @@
   "Move point back to indentation of beginning of line.
 
 Move point to the first non-whitespace character on this line.
-If point is already there, move to the beginning of the line.
-Effectively toggle between the first non-whitespace character and
-the beginning of the line.
-
-If ARG is not nil or 1, move forward ARG - 1 lines first.  If
-point reaches the beginning or end of the buffer, stop there."
-  (interactive "^p")
-  (setq arg (or arg 1))
-
-  ;; Move lines first
-  (when (/= arg 1)
-    (let ((line-move-visual nil))
-      (forward-line (1- arg))))
+If point is already there, move to the beginning of the line."
+  (interactive)
 
   (let ((orig-point (point)))
     (back-to-indentation)
     (when (= orig-point (point))
-      ($move-to-beginning-of-line 1))))
+      (move-beginning-of-line nil))))
 
 (defun $custom-els-path (suffix)
   "Return the path of the custom Emacs Lisp configuration."

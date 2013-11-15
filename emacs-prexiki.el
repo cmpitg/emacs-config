c6f8ea192d568c60dd4d07410328a2c23891c519
Duong H. Nguyen <cmpitg@gmail.com>
2013-11-15 14:41:14 +0700

    Clean up config

--- emacs-prexiki.el
+++ emacs-prexiki.el
@@ -199,36 +199,34 @@
 (require 'popwin)
 (popwin-mode 1)
 
-(push '("\*anything*" :regexp t :height 20) popwin:special-display-config)
-;; M-x anything
+(push '("\*anything*" :regexp t :height 20)         popwin:special-display-config)
+
 (setq anything-samewindow nil)
-(push '("*anything*" :height 20) popwin:special-display-config)
 
-;; M-x dired-jump-other-window
-(push '(dired-mode :position top) popwin:special-display-config)
+(push '("*anything*" :height 20)                    popwin:special-display-config)
 
-;; M-!
-(push "*Shell Command Output*" popwin:special-display-config)
+(push '(dired-mode :position top)                   popwin:special-display-config)
 
-;; M-x compile
-(push '(compilation-mode :noselect t) popwin:special-display-config)
+(push "*Backtrace*"                                 popwin:special-display-config)
+(push "*Shell Command Output*"                      popwin:special-display-config)
+(push '(compilation-mode :noselect t)               popwin:special-display-config)
 
 ;; slime
-(push "*slime-apropos*" popwin:special-display-config)
-(push "*slime-macroexpansion*" popwin:special-display-config)
-(push "*slime-description*" popwin:special-display-config)
-(push '("*slime-compilation*" :noselect t) popwin:special-display-config)
-(push "*slime-xref*" popwin:special-display-config)
-(push '(sldb-mode :stick t) popwin:special-display-config)
-(push 'slime-repl-mode popwin:special-display-config)
-(push 'slime-connection-list-mode popwin:special-display-config)
+(push "*slime-apropos*"                             popwin:special-display-config)
+(push "*slime-macroexpansion*"                      popwin:special-display-config)
+(push "*slime-description*"                         popwin:special-display-config)
+(push '("*slime-compilation*" :noselect t)          popwin:special-display-config)
+(push "*slime-xref*"                                popwin:special-display-config)
+(push '(sldb-mode :stick t)                         popwin:special-display-config)
+(push 'slime-repl-mode                              popwin:special-display-config)
+(push 'slime-connection-list-mode                   popwin:special-display-config)
 
 ;; vc
-(push "*vc-diff*" popwin:special-display-config)
-(push "*vc-change-log*" popwin:special-display-config)
+(push "*vc-diff*"                                   popwin:special-display-config)
+(push "*vc-change-log*"                             popwin:special-display-config)
 
 ;; undo-tree
-(push '(" *undo-tree*" :width 0.3 :position right) popwin:special-display-config)
+(push '(" *undo-tree*" :width 0.3 :position right)  popwin:special-display-config)
 
 ;;
 ;; Highlight brackets

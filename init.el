9f8fa7fed5a751816b1244517c8b06e8a6dece1e
Duong H. Nguyen <cmpitg@gmail.com>
2013-11-24 18:33:26 +0700

    Add license header

--- init.el
+++ init.el
@@ -1,21 +1,32 @@
 ;; -*- mode: emacs-lisp -*-
 
+;;
+;; Copyright (C) 2012-2013 Duong H. Nguyen <cmpitgATgmaildotcom>
+;;
+;; bogoengine is free software: you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free
+;; Software Foundation, either version 3 of the License, or (at your option)
+;; any later version.
+;;
+;; bogoengine is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+;; more details.
+;;
+;; You should have received a copy of the GNU General Public License along
+;; with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
+
 (defun -load-files-if-exists- (&rest paths)
   "Load files when they exists."
   (dolist (file-path paths)
    (when (file-exists-p file-path)
      (load file-path))))
 
-;; Pre-customization
-
-(-load-files-if-exists- "~/emacs-custom-foremost.el")
-
-;; Main
-
-(-load-files-if-exists- "~/emacs-config/emacs-cmpitg-config/emacs-environment.el"
+(-load-files-if-exists- "~/emacs-custom-foremost.el"  ; Foremost
+                        "~/emacs-config/global-vars.el"
                         "~/emacs-config/package-list.el"
-                        "~/emacs-config/emacs-prexiki.el")
-
-;; All user-defined customization goes here
-
-(-load-files-if-exists- "~/emacs-custom.el")
+                        "~/emacs-config/emacs-cmpitg-config/emacs-environment.el"
+                        "~/emacs-config/emacs-prexiki.el"
+                        "~/emacs-custom.el"           ; User-defined customization
+                        )

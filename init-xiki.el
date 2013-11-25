eee31a8ae320f3496cc09442a878144c70e46a7a
Duong H. Nguyen <cmpitg@gmail.com>
2013-11-25 17:09:44 +0700

    Move main Xiki config to another file

--- init-xiki.el
+++ init-xiki.el
@@ -24,56 +24,6 @@
 (-load-files-if-exists- "~/emacs-custom-foremost.el"
                         "~/emacs-config/global-vars.el"
                         "~/emacs-config/package-list.el"
-                        "~/emacs-config/main.el")
-
-(global-unset-key (kbd "C-/"))
-
-; Load el4r, which loads Xiki
-(add-to-list 'load-path *xiki-path*)
-(require 'el4r)
-(el4r-boot)
-
-;;; Xiki environment
-
-;; Let me press "y", instead of type "yes"
-(fset 'yes-or-no-p 'y-or-n-p)
-
-;; C-k kills whole line if at beginning
-(setq kill-whole-line nil)
-
-;; Make tabe into spaces when you type them
-(setq-default indent-tabs-mode nil)
-;; Display existing tabs as 2 characters wide
-(setq-default tab-width 2)
-
-;; No new frame for ediff
-(setq ediff-window-setup-function 'ediff-setup-windows-plain)
-;; Show ediff views side-by-side
-(setq ediff-split-window-function 'split-window-horizontally)
-
-;; Set default font
-(set-face-attribute 'default nil
-  :height 110
-  :family "Monaco")
-
-;; Save cursor location upon closing files, and restore upon reopening
-(require 'saveplace)
-(setq-default save-place t)
-
-;; Don't create #... files when editing
-(setq make-backup-files nil)
-
-;; Numbering lines
-(require 'linum)
-(global-linum-mode 1)
-
-;;; fill-column
-(setq-default fill-column 78)
-(set-fill-column 78)
-
-;; Change cursor type
-;;; 'hbar 'bar or 'box
-(set-default 'cursor-type 'bar)
-
-;;; User-defined customization
-(-load-files-if-exists- "~/emacs-custom.el")
+                        "~/emacs-config/main.el"
+                        "~/emacs-config/main-xiki.el"
+                        "~/emacs-custom.el")
---
+++ main-xiki.el
@@ -0,0 +1,65 @@
+;;
+;; Copyright (C) 2012-2013 Duong H. Nguyen <cmpitgATgmaildotcom>
+;;
+;; bogoengine is free software: you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free
+;; Software Foundation, either version 3 of the License, or (at your option)
+;; any later version.
+;;
+;; bogoengine is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+;; more details.
+;;
+;; You should have received a copy of the GNU General Public License along
+;; with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
+
+(global-unset-key (kbd "C-/"))
+
+; Load el4r, which loads Xiki
+(add-to-list 'load-path *xiki-path*)
+(require 'el4r)
+(el4r-boot)
+
+;;; Xiki environment
+
+;; Let me press "y", instead of type "yes"
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; C-k kills whole line if at beginning
+(setq kill-whole-line nil)
+
+;; Make tabe into spaces when you type them
+(setq-default indent-tabs-mode nil)
+;; Display existing tabs as 2 characters wide
+(setq-default tab-width 2)
+
+;; No new frame for ediff
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+;; Show ediff views side-by-side
+(setq ediff-split-window-function 'split-window-horizontally)
+
+;; Set default font
+(set-face-attribute 'default nil
+  :height 110
+  :family "Monaco")
+
+;; Save cursor location upon closing files, and restore upon reopening
+(require 'saveplace)
+(setq-default save-place t)
+
+;; Don't create #... files when editing
+(setq make-backup-files nil)
+
+;; Numbering lines
+(require 'linum)
+(global-linum-mode 1)
+
+;;; fill-column
+(setq-default fill-column 78)
+(set-fill-column 78)
+
+;; Change cursor type
+;;; 'hbar 'bar or 'box
+(set-default 'cursor-type 'bar)

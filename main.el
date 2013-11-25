ae7dc4d0de163ce3ce0e074f98101d8cf3fa094c
Duong H. Nguyen <cmpitg@gmail.com>
2013-11-25 23:00:09 +0700

    Use ElDoc mode

--- main.el
+++ main.el
@@ -494,6 +494,10 @@
  'paredit-backward-delete
  'paredit-close-round)
 
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
+
 ;;; Use with SLIME REPL
 (add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1)))
 

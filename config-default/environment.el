c74d7def7aab6def8647ee20f0b3b583facb1309
Duong H. Nguyen <cmpitg@gmail.com>
2013-11-25 14:51:49 +0700

    Clean up environment.el

--- config-default/environment.el
+++ config-default/environment.el
@@ -1,68 +1,19 @@
-;; read-quoted-char-radix (10 or 16)
-
-;; Set the appropriate frame size
-; (set-frame-size-according-to-resolution)
-
-;(color-theme-molokai)
-;(color-theme-zenburn)
-;(color-theme-textmate)
-;(color-theme-twilight)
-;(inspiration-144382)
-;(inspiration-648409)
-;(inspiration-990434)
-;(color-theme-charcoal-black)
-;(color-theme-calm-forest)
-
-;; Using system font
-
-(setq font-use-system-font t)
-
-;; Custom variables
-
-(custom-set-variables
- ;; '(column-number-mode t)
- ;; '(display-battery-mode t)
- ;; '(display-time-mode t)
- ;; '(size-indication-mode t)
- '(face-font-family-alternatives (quote (("Monaco" "Consolas" "Monospace")
-                                         ("Monaco" "Consolas" "CMU Typewriter Text" "fixed")
-                                         ("Geneva" "Sans Serif" "helv" "helvetica" "arial" "fixed")
-                                         ("helv" "helvetica" "arial" "fixed"))))
- '(safe-local-variable-values (quote ((Syntax . ANSI-Common-Lisp) (Base . 10) (encoding . utf-8))))
- '(show-paren-mode t)
- '(uniquify-buffer-name-style (quote forward) nil (uniquify)))
-
-;; Custom fonts
-
-(custom-set-faces
- '(default ((t (:inherit nil
-                         :stipple nil
-                         :inverse-video nil
-                         :box nil
-                         :strike-through nil
-                         :overline nil
-                         :underline nil
-                         :slant normal
-                         :weight normal
-                         ;; :height 98
-                         :width normal
-                         :foundry "unknown"
-                         :family "Monaco"))))
- ;; '(mode-line ((t (:background "grey75"
- ;;                              :foreground "#3d3d3d"
- ;;                              :inverse-video t
- ;;                              :box (:line-width 1 :color "#000000" :style released-button)
- ;;                              :slant normal
- ;;                              :weight normal
- ;;                              :height 100
- ;;                              :family "Geneva"))))
- ;; '(mode-line ((t (:inverse-video t
- ;;                                 :slant normal
- ;;                                 :weight normal
- ;;                                 :height 100
- ;;                                 :family "Geneva"))))
-
- '(rst-level-1-face ((t (:embolden t))) t))
+;;
+;; Copyright (C) 2012-2013 Duong H. Nguyen <cmpitgATgmaildotcom>
+;;
+;; bogoengine is free software: you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free
+;; Software Foundation, either version 3 of the License, or (at your option)
+;; any later version.
+;;
+;; bogoengine is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+;; more details.
+;;
+;; You should have received a copy of the GNU General Public License along
+;; with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
 
 ;; yes/no questions become y/n questions
 (defalias 'yes-or-no-p 'y-or-n-p)
@@ -81,11 +32,6 @@
 
 ;; Expand tabs to spaces
 (setq-default indent-tabs-mode nil)
-
-;; Show line number
-(line-number-mode 1)
-;; Set linumber format
-(setq linum-format "%d ")
 
 ;; Show column number
 (column-number-mode 1)
@@ -138,6 +84,7 @@
 ;; Numbering lines
 (require 'linum)
 (global-linum-mode 1)
+(setq linum-format "%d ")
 
 ;; Show the battery indicator
 (display-battery-mode 1)
@@ -149,10 +96,6 @@
 ;;; fill-column
 (setq-default fill-column 78)
 (set-fill-column 78)
-
-;;; Automatically turn on auto-fill and refill mode
-;; (add-hook 'text-mode-hook 'turn-on-auto-fill)
-;; (add-hook 'text-mode-hook (lambda () (refill-mode 1)))
 
 (require 'desktop)
 (require 'tar-mode)
@@ -177,11 +120,6 @@
 
 ;; Echo when trying to kill in a read-only buffer
 (setq kill-read-only-ok t)
-
-;; Change cursor type
-;;(set-default 'cursor-type 'hbar)
-(set-default 'cursor-type 'bar)
-;;(set-default 'cursor-type 'box)
 
 ;;; Blink cursor
 (blink-cursor-mode t)

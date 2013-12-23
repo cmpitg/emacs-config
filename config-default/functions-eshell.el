7c431a73465a34a488ef1f2507628789a22ae79f
Duong H. Nguyen <cmpitg@gmail.com>
2013-12-23 23:10:46 +0700

    Remove redundant comments

--- config-default/functions-eshell.el
+++ config-default/functions-eshell.el
@@ -14,10 +14,6 @@
 ;; You should have received a copy of the GNU General Public License along
 ;; with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;;
-;;
-;; TODO
-;; Note: This file shouldn't be loaded separatedly
-;;
 
 (defun $eshell-history ()
   "Display eshell commands as with M-x.  The selected command is

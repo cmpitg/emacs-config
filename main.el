0f089b8a732476f4b4dc25aa176a6b80b21b7240
NgaNguyenDuy <kyo1508@gmail.com>
2014-05-01 23:52:21 +0700

    update main

--- main.el
+++ main.el
@@ -129,6 +129,20 @@
 
 
 ;;
+;; web-mode config
+;;
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode)) 
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode)) 
+(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode)) 
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode)) 
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode)) 
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode)) 
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
+
+;;
 ;; Load all but disabled packages (defined in `package-list.el`)
 ;;
 
